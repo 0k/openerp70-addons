7144a6ce94df8b06b067489a36891cedc3486da3
ddrapeau <david.drapeau@gmail.com>
2012-12-23 11:16:52 +0100

    change field name vision by goal in project

--- ddrapeau_project_scrum/project.py
+++ ddrapeau_project_scrum/project.py
@@ -8,7 +8,7 @@ class projectProjectInehrit(osv.osv):
         'sprint_size': fields.integer('Sprint Days', help="Number of days allocated for sprint"),
         'is_scrum': fields.boolean("Is it a Scrum Project ?"),
         'product_owner_id': fields.many2one('res.users', "Product Owner"),
-        'vision' : fields.text("Vision", help="The document that includes the project, jointly between the team and the customer"),
+        'goal' : fields.text("Goal", help="The document that includes the project, jointly between the team and the customer"),
         'done' : fields.char("What is the \"Done\"?", size=128),
     }
     _defaults = {
