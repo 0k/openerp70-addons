985d023c66e20d791e4c8da20fb4a07b570f8519
ddrapeau <david.drapeau@gmail.com>
2012-12-24 10:09:39 +0100

    delete done and sprint size from project.project

--- ddrapeau_project_scrum/project.py
+++ ddrapeau_project_scrum/project.py
@@ -5,14 +5,14 @@ from tools.translate import _
 class projectProjectInehrit(osv.osv):
     _inherit = 'project.project'
     _columns = {
-        'sprint_size': fields.integer('Sprint Days', help="Number of days allocated for sprint"),
+        #'sprint_size': fields.integer('Sprint Days', help="Number of days allocated for sprint"),
         'is_scrum': fields.boolean("Is it a Scrum Project ?"),
         'product_owner_id': fields.many2one('res.users', "Product Owner"),
         'goal' : fields.text("Goal", help="The document that includes the project, jointly between the team and the customer"),
-        'done' : fields.char("What is the \"Done\"?", size=128),
+        #'done' : fields.char("What is the \"Done\"?", size=128),
     }
     _defaults = {
-        'sprint_size': 10,
+        #'sprint_size': 10,
         'is_scrum': True,
     }
 
