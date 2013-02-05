f6691ec4371e3ce7214e6412c29f28d0ae5a5de4
ddrapeau <david.drapeau@gmail.com>
2013-02-05 08:24:47 +0100

    optimize view project for make difference between Project Manager and Scrum Master

--- project_scrum/project.py
+++ project_scrum/project.py
@@ -6,6 +6,7 @@ class projectProjectInehrit(osv.osv):
     _inherit = 'project.project'
     _columns = {
         'is_scrum': fields.boolean("Is it a Scrum Project ?"),
+        'scrum_master_id': fields.many2one('res.users', 'Scrum Master', help="The person who is maintains the processes for the product"),
         'product_owner_id': fields.many2one('res.users', "Product Owner"),
         'goal' : fields.text("Goal", help="The document that includes the project, jointly between the team and the customer"),
     }
