6ad9a8e8b0dc6089716d1bf31b63b9e1461db495
ddrapeau <david.drapeau@gmail.com>
2013-03-15 11:41:10 +0100

    creation du module portal account

---
+++ portal_account/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Portal Account",
+    'version': "0.1",
+    'category': 'Tools',
+    'complexity': 'easy',
+    'author': "EL2DE SARL <david.drapeau@el2de.com>",
+    'website': "http://www.el2de.com",
+    'description': """
+    
+    """,
+    
+    'depends': ['account','portal'],
+    'data': [
+        #'security/portal_account.xml',
+        #'security/ir.model.access.csv',
+        'view/portal_account_view.xml',
+    ],
+    
+    'installable': True,
+    'auto_install': False,
+    'web': True,
+}
